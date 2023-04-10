853c36974e8198bab29a68e0010a2df8359535a1
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-04-10 14:00:35 +0900

    [Bronze V] Title: A/B, Time: 212 ms, Memory: 17772 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double A = (double)sc.nextInt();
+        double B = (double)sc.nextInt();
+        System.out.println(A/B);
+    }    
+}
