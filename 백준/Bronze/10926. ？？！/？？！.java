608443bf0d2edf155f1f9e80e45a7010f7e87766
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-04-10 20:42:35 +0900

    [Bronze V] Title: ??!, Time: 200 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        sb.append(sc.next()).append("??!");
+        System.out.println(sb); 
+    }
+}
