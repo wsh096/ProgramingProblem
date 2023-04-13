dee7ab676522ad51c9b44d24a522f58cffa965a6
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-04-14 00:07:13 +0900

    [Bronze IV] Title: 숫자의 합, Time: 212 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main{
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+        char[] number = sc.next().toCharArray();
+        int sum = 0;
+        for(char c : number)
+            sum+=c-'0';
+    System.out.println(sum);
+    }
+}
