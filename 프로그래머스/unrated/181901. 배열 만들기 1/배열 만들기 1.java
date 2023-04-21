6da724d202bf75a8c97b56a5f29c6dfd308766ce
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-04-21 21:00:14 +0900

    [unrated] Title: 배열 만들기 1, Time: 5.59 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.java
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.java
@@ -1,7 +1,6 @@
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 class Solution {
     public int[] solution(int n, int k) {
-        return IntStream.rangeClosed(1,n).filter(num->num%k ==0).toArray();
+        int[] answer = {};
+        return answer;
     }
 }
