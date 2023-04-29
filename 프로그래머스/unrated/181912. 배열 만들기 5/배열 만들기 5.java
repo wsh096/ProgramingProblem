e6e47749398226f5fdb42b29456899d0458b78dc
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-04-29 21:13:44 +0900

    [unrated] Title: 배열 만들기 5, Time: 9.52 ms, Memory: 87.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.java
@@ -0,0 +1,24 @@
+//1. 단순 풀이(마지막 정답 부분도 스트림 안 쓰는게 성능 상은 더 좋음)
+
+// import java.util.List;
+// import java.util.LinkedList;
+// class Solution {
+//     public int[] solution(String[] intStrs, int k, int s, int l) {
+//         List<Integer> answer = new LinkedList<>();
+//         for(int i = 0 ; i < intStrs.length; i++){
+//             int target = Integer.parseInt(intStrs[i].substring(s,s+l));
+//             if(target > k) answer.add(target);        
+//         }
+//         return answer.stream().mapToInt(Integer::intValue).toArray();
+//     }
+// }
+
+//2. 스트림을 활용한 풀이.
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+class Solution{
+    public int[] solution(String [] intStrs, int k, int s, int l){
+        return Arrays.stream(intStrs).map(str -> Integer.parseInt(str.substring(s,s+l))).filter(num-> num > k).collect(Collectors.toList()).stream().mapToInt(Integer::intValue).toArray();
+    }
+}
