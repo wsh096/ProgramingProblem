f4ee1fce9fcd55bbed49ff40ae6a7af10476767c
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-05-01 00:05:06 +0900

    [Bronze IV] Title: 기찍 N, Time: 1172 ms, Memory: 33752 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int i = T; i >= 1; i--){
+            System.out.println(i);
+        }
+    }
+}
