b606b189525c95c3e2a41f41a6f6bc983e7502fe
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-05-04 21:00:56 +0900

    [unrated] Title: 순서 바꾸기, Time: 2.28 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,24 @@
+// //1. 기본 풀이
+
+// class Solution {
+//     public int[] solution(int[] num_list, int n) {
+//         int[] answer = new int[num_list.length];
+//         int idx = 0;
+//         for(int i = n; i < num_list.length; i++){
+//             answer[idx++] = num_list[i];
+//         }
+//         for(int i = 0; i < n; i++){
+//             answer[idx++] = num_list[i];
+//         }
+//         return answer;
+//     }
+// }
+
+//2. 스트림 풀이
+import java.util.stream.IntStream;
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] num_list, int n) {         
+          return IntStream.concat(Arrays.stream(Arrays.copyOfRange(num_list,n,num_list.length)),Arrays.stream(Arrays.copyOfRange(num_list,0,n))).toArray();
+    }
+}
