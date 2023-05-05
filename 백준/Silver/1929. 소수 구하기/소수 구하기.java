d53d1eddd5f05ac2bce6f15b2d7caceb21da69e0
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-05-05 16:40:06 +0900

    [Silver III] Title: 소수 구하기, Time: 324 ms, Memory: 20640 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -2,19 +2,21 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        
         int M = sc.nextInt();
         int N = sc.nextInt();
-        boolean[] frame = new boolean[N+1];
-        frame[0] = true;
-        frame[1] = true;
-        for(int i = 2; i*i <= N; i++){
-            if(!frame[i])
-            for(int j = i*i; j <= N; j += i)
-                    frame[j] = true; 
-            }
         
-        for(int i = M; i <= N; i++)
-            if(!frame[i]) System.out.println(i);
+        boolean[] frame = new boolean[N+5];
+        
+        for(int i = 2; i <= N; i++){
+            if(frame[i]) continue;
+            if(i >= M) sb.append(i).append("\n");
+            for(int j = i+i; j <= N; j += i)
+                    frame[j] = true; 
+        }
+        
+      System.out.println(sb.toString());
         
         sc.close();
     }
