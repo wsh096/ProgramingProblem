23778dd40d71400183ebe8e57fe5d96c3e34d726
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-05-05 13:47:47 +0900

    [unrated] Title: 요격 시스템, Time: 0.45 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
@@ -1,11 +1,14 @@
 import java.util.Arrays;
-
+import java.util.Comparator;
 class Solution {
     public int solution(int[][] targets) {
         
-        Arrays.sort(targets, (o1, o2) -> {
-            if(o1[1] == o2[1]) return o1[0] - o2[0];
+        Arrays.sort(targets, new Comparator<int[]>() { 
+            public int compare(int [] o1, int[] o2){
+            if(o1[1] == o2[1]) 
+                return o1[0] - o2[0];
             return o1[1] - o2[1];
+            }
         });
         
         int end = targets[0][1];
