20c7a78028b615b5477714cd167b4edff50cec06
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-05-06 20:17:38 +0900

    [Bronze II] Title: 피보나치 수, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.java
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -1,15 +1,16 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] dp = new int [N+2];
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int [N+1];
         dp[0] = 0;
         dp[1] = 1;
-        dp[2] = 1;
     
-        for(int i = 3; i <= N; i++){
+        for(int i = 2; i <= N; i++){
             dp[i] = dp[i-2] + dp[i-1];
         }
         System.out.println(dp[N]);
