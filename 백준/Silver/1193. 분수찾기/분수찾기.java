50fdac60875fbe2895f12970ce7274f1fbbb5377
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-05-07 21:01:51 +0900

    [Silver V] Title: 분수찾기, Time: 148 ms, Memory: 16068 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.java
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -8,10 +8,9 @@ public class Main {
         int N = Integer.parseInt(br.readLine()), cross = 1, prevs = 0;
         while(true){
             if(N <= cross + prevs){
-                //대각선이 홀수면, 분자감소, 분모증가.
                 if(cross % 2 == 1)
                     System.out.println((cross-(N-prevs-1))+"/"+(N-prevs));
-                else//대각선이 짝수면, 분자증가, 분모감소.
+                else
                     System.out.println((N-prevs)+"/"+(cross-(N-prevs-1)));
                 break;
             }else
