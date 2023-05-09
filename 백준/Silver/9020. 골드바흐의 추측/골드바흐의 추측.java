d35a7db0385bb9ebf85143b902abed0da7afd98f
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-05-09 22:44:49 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 156 ms, Memory: 15288 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.java
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.java
@@ -1,13 +1,11 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         
         int T = Integer.parseInt(br.readLine());
         boolean[] prime = new boolean[10001]; prime[0] = true; prime[1] = true;
@@ -16,22 +14,20 @@ public class Main{
             for(int j = i*i; j < prime.length; j+=i)
             if(!prime[j]) prime[j] = true;
         }
+        
         while(T-- > 0){
-            int[] ints = {0,10_001};
-            int A = Integer.parseInt(br.readLine());
-            for(int i = A-1; i >= 2; i--){
-                if(!prime[i] && !prime[A-i] && Math.abs(i - (A-i)) <Math.abs(ints[1] - ints[0])){
-                    ints = new int[] {A-i, i};
-                }
-            }
-            bw.write(String.valueOf(ints[0]));
-            bw.write(" ");
-            bw.write(String.valueOf(ints[1]));
-            bw.newLine();
-        }
 
-        bw.flush();
-        bw.close();
+            int A = Integer.parseInt(br.readLine());
+            int first = A/2;
+            int second = A/2;
+
+            while(prime[first]||prime[second]){
+                first--; second++;
+            }
+
+            sb.append(first).append(" ").append(second).append("\n");
+        }
+        System.out.println(sb);
         br.close();
     }
 }
