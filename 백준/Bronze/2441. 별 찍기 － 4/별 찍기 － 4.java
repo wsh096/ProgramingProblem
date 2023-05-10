9cb1571d11ba36e603203cb43c1487d139075911
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-05-11 00:11:30 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 124 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for(int i = 0; i < T; i++){
+           for(int j = i; j < T; j++){
+           if(j==i) for(int k = 0; k < i; k++) sb.append(" ");
+               sb.append("*");
+            }
+            sb.append("\n");
+        }
+     System.out.println(sb);
+    }
+}
