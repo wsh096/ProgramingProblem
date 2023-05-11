5693a68a5eb2df418fba607bd5f49019a0cc8847
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-05-11 23:22:21 +0900

    [Bronze V] Title: 등록, Time: 116 ms, Memory: 13836 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        StringBuffer sb = new StringBuffer();
+        sb.append(134).append("\n").append("wsh096");
+            System.out.println(sb);
+    }
+ } 
