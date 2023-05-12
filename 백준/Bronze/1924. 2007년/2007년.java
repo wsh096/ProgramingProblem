9d52e24fd6dc7e431051de2a56ac303851c008ff
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-05-13 01:29:52 +0900

    [Bronze I] Title: 2007년, Time: 128 ms, Memory: 14432 KB -BaekjoonHub

--- 백준/Bronze/1924. 2007년/2007년.java
+++ 백준/Bronze/1924. 2007년/2007년.java
@@ -1,4 +1,4 @@
-import java.util.Date;
+import java.time.LocalDate;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
@@ -7,9 +7,6 @@ public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
-        Date x = new Date();
-        x.setYear(107); x.setMonth(Integer.parseInt(input[0])-1);
-        x.setDate(Integer.parseInt(input[1]));
-        System.out.println(x.toString().substring(0,3).toUpperCase());
+        System.out.println(LocalDate.of(2007, Integer.parseInt(input[0]), Integer.parseInt(input[1])).getDayOfWeek().toString().substring(0,3));
     }
 }
