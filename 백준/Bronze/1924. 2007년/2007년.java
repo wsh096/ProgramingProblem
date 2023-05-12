875bc0c0e57e18f096fea12a6435bad6e2f43350
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-05-13 01:35:33 +0900

    [Bronze I] Title: 2007년, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/1924. 2007년/2007년.java
+++ 백준/Bronze/1924. 2007년/2007년.java
@@ -1,12 +1,30 @@
-import java.time.LocalDate;
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main{
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        System.out.println(LocalDate.of(2007, Integer.parseInt(input[0]), Integer.parseInt(input[1])).getDayOfWeek().toString().substring(0,3));
-    }
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
+
+		int[] month = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		String[] dayOfWeek = { "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT" };
+
+		// x월 전까지 다 더하기
+		int result = 0;
+		for (int i = 0; i < x - 1; i++) {
+			result += month[i];
+		}
+
+		// x달의 추가적 y일수 다 더하기
+		result += y;
+		System.out.println(dayOfWeek[result % 7]);
+
+	}
+
 }
