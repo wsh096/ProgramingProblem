db4bda455b951b5d49addb2e3c424d5ba8fbd0dc
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-05-22 21:23:05 +0900

    [unrated] Title: 두 원 사이의 정수 쌍, Time: 17.29 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
+++ 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -1,16 +1,50 @@
-class Solution {
-    public long solution(int r1, int r2) {
-       
-        long answer = 0;
+public class Solution {
+    public int solution(int r1, int r2) {
+        int result = 0;
         
-        for(long i = 1L; i <= r2; i++){//r2가 되었을 때는 두 수 모두 0이기 때문에 무의미하지만, +1이 되어 값정산
-            //newr1의 밖을 탐색하는 것이기 때문에 ceil
-            int newr1 = (int)Math.ceil(Math.sqrt((long)r1*r1-i*i)); //피타고라스정리를 통한 다른 변 길이 구하기.
-            //r2원의 내부를 탐색하는 것이기 때문에 floor
-            int newr2 = (int)Math.floor(Math.sqrt((long)r2*r2-i*i)); //음수의 sqrt는 0.
-            answer += newr2-newr1+1;
+        // 두 원의 중심 사이의 거리 계산
+        double distance = Math.sqrt(Math.pow(0, 2) + Math.pow(0, 2));
+        
+        // 두 원의 반지름 합과 차 계산
+        int radiusSum = r1 + r2;
+        int radiusDiff = r2 - r1;
+        
+        // 두 원이 겹치지 않는 경우
+        if (distance > radiusSum) {
+            result = 0;
         }
-
-           return answer*4;
+        // 한 원이 다른 원 안에 포함되는 경우
+        else if (distance + r1 < r2) {
+            result = 0;
+        }
+        // 두 원이 완전히 겹치는 경우
+        else if (distance == 0 && r1 == r2) {
+            result = -1;
+        }
+        // 두 원이 접하는 경우
+        else if (distance == radiusSum || distance + r1 == r2) {
+            result = 1;
+        }
+        // 두 원이 두 점에서 만나는 경우
+        else {
+            result = countIntegerPoints(distance, radiusDiff);
+        }
+        
+        return result;
+    }
+    
+    // 두 원 사이의 정수 좌표 개수 세기
+    private int countIntegerPoints(double distance, int radiusDiff) {
+        int count = 0;
+        
+        for (int x = 1; x < radiusDiff; x++) {
+            int y = (int) Math.sqrt(radiusDiff * radiusDiff - x * x);
+            
+            if (y * y + x * x == radiusDiff * radiusDiff) {
+                count += 4;
+            }
+        }
+        
+        return count;
     }
 }
