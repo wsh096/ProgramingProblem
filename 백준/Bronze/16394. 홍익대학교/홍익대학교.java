cd8dbfdcc524fceb6661bf8283a7343200d94bd9
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-05-23 23:21:41 +0900

    [Bronze V] Title: 홍익대학교, Time: 124 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Bronze/16394. 홍익대학교/홍익대학교.java
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -1,7 +1,9 @@
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 public class Main{
-    public static void main(String[] args){
-       Scanner sc = new Scanner(System.in);
-       System.out.println(sc.nextInt()-1946); 
+    public static void main(String[] args) throws IOException{
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       System.out.println(Integer.parseInt(br.readLine())-1946); 
     }
 }
