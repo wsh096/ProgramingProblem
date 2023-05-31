22b206c66bbca8c112b0e1112b272f1f6e4f4c9f
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-05-31 19:51:41 +0900

    [Gold V] Title: 토마토, Time: 304 ms, Memory: 65936 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,96 +1,99 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Main {
-    static int[][] box;
-    static int[][] days;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-    static int n, m;
+
+    private static final int[] DIRECTION_X = {0, 0, -1, 1}; // 상,하, 좌,우
+    private static final int[] DIRECTION_Y = {-1, 1, 0, 0};
+
+    private static int M; // 가로
+    private static int N; // 세로
+    private static int[][] storage;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        m = Integer.parseInt(input[0]);
-        n = Integer.parseInt(input[1]);
-        box = new int[n][m];
-        days = new int[n][m];
-
-        for (int i = 0; i < n; i++) {
-            String[] row = br.readLine().split(" ");
-            for (int j = 0; j < m; j++) {
-                box[i][j] = Integer.parseInt(row[j]);
-                days[i][j] = -1; // 초기값을 -1로 설정 0과 구분
-            }
-        }
-
-        int answer = bfs();
-
-        if (isAll()) {
-            System.out.println(answer);
-        } else {
-            System.out.println(-1);
-        }
-
-        br.close();
+        initStorage();
+        bfs();
+        System.out.print(answer());
     }
 
-    private static int bfs() {
-        Queue<Point> queue = new LinkedList<>();
+    private static void initStorage() throws IOException {
+        M = readInt(); // 가로
+        N = readInt(); // 세로
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (box[i][j] == 1) {
-                    queue.add(new Point(i, j));
-                    days[i][j] = 0;
+        storage = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                storage[i][j] = readInt();
+            }
+        }
+    }
+
+    private static void bfs() {
+        Queue<int[]> queue = new LinkedList<>();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (storage[i][j] == 1) { // 익은 토마토이면 큐에 좌표를 삽입
+                    queue.offer(new int[]{j, i});
                 }
             }
         }
-
-        int maxDays = 0;
 
         while (!queue.isEmpty()) {
-            Point curr = queue.poll();
+            int[] ripeTomato = queue.poll();
+            int x = ripeTomato[0];
+            int y = ripeTomato[1];
 
             for (int i = 0; i < 4; i++) {
-                int nx = curr.x + dx[i];
-                int ny = curr.y + dy[i];
+                int nearX = x + DIRECTION_X[i];
+                int nearY = y + DIRECTION_Y[i];
 
-                if (isValid(nx, ny) && box[nx][ny] == 0 && days[nx][ny] == -1) {
-                    queue.add(new Point(nx, ny));
-                    days[nx][ny] = days[curr.x][curr.y] + 1;
-                    maxDays = Math.max(maxDays, days[nx][ny]);
+                if (nearX < 0 || nearY < 0 || nearX >= M || nearY >= N) { // 인덱스 범위 밖을 벗어나면 무시
+                    continue;
+                }
+
+                if (storage[nearY][nearX] == -1) { // 빈 공간일 경우 무시
+                    continue;
+                }
+                if (storage[nearY][nearX] == 0) { // 안 익은 토마토일 경우 숙성 처리. 값을 늘려서 숙성일을 누적시킨다.
+                    storage[nearY][nearX] = storage[y][x] + 1;
+                    queue.offer(new int[]{nearX, nearY});
                 }
             }
         }
-
-        return maxDays;
     }
 
-    private static boolean isValid(int x, int y) {
-        return x >= 0 && x < n && y >= 0 && y < m;
-    }
-
-    private static boolean isAll() {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (box[i][j] == 0 && days[i][j] == -1) {
-                    return false;
+    private static int answer() {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (storage[i][j] > max) {
+                    max = storage[i][j]; // 최댓값 갱신
+                }
+                if (storage[i][j] == 0) {
+                    return -1;
                 }
             }
         }
-        return true;
+       return max-1; // 모두 익었을 경우. 최댓값-1이 모두 숙성하는데 소요되는 날짜
     }
 
-    static class Point {
-        int x, y;
+    private static int readInt() throws IOException {
+        int value = 0;
+        boolean negative = false;
 
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
+        int input;
+        while (true) {
+            input = System.in.read();
+            if (input == ' ' || input == '\n') {
+                return (negative) ? -value : value;
+            } else if (input == '-') {
+                negative = true;
+            } else {
+                value = value * 10 + (input - 48);
+            }
         }
     }
 }
