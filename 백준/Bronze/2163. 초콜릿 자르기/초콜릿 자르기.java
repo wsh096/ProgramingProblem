d4400452d6358bd7db696e54228741e73d1e1799
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-06-03 17:06:26 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 136 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int A = Integer.parseInt(input[0]);
+        int B = Integer.parseInt(input[1]);
+            
+        System.out.println((B-1)*A+A-1);
+        br.close();
+    }
+}
