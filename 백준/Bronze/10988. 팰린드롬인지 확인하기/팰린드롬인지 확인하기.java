20eb67879d0d2b2fd184d2c8e674d5029347964b
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-06-06 09:25:26 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 124 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -1,24 +1,10 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.IOException;
-public class Main{
-   public static void main(String[] args) throws IOException{
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       char[] chars = br.readLine().toCharArray();
-       int answer = 1;
-       int left = 0;
-       int right = chars.length-1;
-       while(left < right){
-           if(chars[left] == chars[right]){
-               left++;
-               right--;
-           }else{
-               answer=0;
-               break;
-           }
-       }
-           
-           System.out.println(answer);
-       br.close();
-   } 
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
+        System.out.println(word.contentEquals(new StringBuilder(word).reverse()) ? 1 : 0);
+    }
 }
