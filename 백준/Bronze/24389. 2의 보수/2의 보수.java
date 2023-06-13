f2b9668101b4c87c54a0b0cec6d9c27a3af09f41
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-06-13 17:44:26 +0900

    [Bronze I] Title: 2의 보수, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Bronze/24389. 2의 보수/2의 보수.java
+++ 백준/Bronze/24389. 2의 보수/2의 보수.java
@@ -5,16 +5,16 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();//이렇게 다 따로 받아야 오류가 생기지 않음!
-        int A = Integer.parseInt(input);
+
+        int A = Integer.parseInt(br.readLine());
         int answer  = 0;
 
-        String B = Integer.toBinaryString(A);
-        String C = Integer.toBinaryString(-A);
-        int idx = C.length()-1;
+        char[] B = Integer.toBinaryString(A).toCharArray();
+        char[] C = Integer.toBinaryString(-A).toCharArray();
+        int idx = C.length-1;
 
-        for(int i = B.length()-1; i >= 0; i--){
-            if(B.charAt(i) != C.charAt(idx--)){
+        for(int i = B.length-1; i >= 0; i--){
+            if(B[i] != C[idx--]){
                 answer++;
             }
         }
