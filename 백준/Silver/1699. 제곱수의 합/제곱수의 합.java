874c23f875b4383254566fba2b1626e208aa0225
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-06-21 16:38:37 +0900

    [Silver II] Title: 제곱수의 합, Time: 124 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
@@ -2,20 +2,32 @@ import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 
-public class Main{
-    public static void main(String[] args) throws IOException{
+public class Main {
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
-       
-        int [] dp = new int[N+1];
-        for(int i = 1; i <= N; i++){
-            dp[i] = i;
-            for(int j = 1; j * j <= i; j++){
-                dp[i] = Math.min(dp[i],dp[i-j*j]+1);
+        System.out.println(lagrange(N));
+    }
+
+    public static int lagrange(int n) {
+        int a = (int)Math.sqrt(n);
+        if ( a*a == n)
+            return 1;
+
+        while (n % 4 == 0)
+            n = n / 4;
+
+        if (n % 8 == 7)
+            return 4;
+
+        for (int i = 0; i <= a; i++) {
+            int j = (int) Math.sqrt(n - i * i);
+            if (j * j == n - i * i) {
+                return 2;
             }
         }
-        
-        System.out.println(dp[N]);
-        br.close();
+        return 3;
     }
 }
