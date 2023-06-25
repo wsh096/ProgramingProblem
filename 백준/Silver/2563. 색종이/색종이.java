98155ca5ac1d155a11b53aac85b4ed2dd20c81da
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-06-25 15:10:48 +0900

    [Silver V] Title: 색종이, Time: 124 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -2,12 +2,12 @@ import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 public class Main{
-    static boolean[][] board = new boolean[101][101];
+
 public static void main(String[] args) throws IOException{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int answer = 0;
     int N = Integer.parseInt(br.readLine());
-    
+    boolean[][] board = new boolean[101][101];
     for(int i = 0 ; i < N; i++){
       String[] input = br.readLine().split(" "); 
         int X = Integer.parseInt(input[0]);
