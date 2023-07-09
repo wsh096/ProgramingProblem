581fe4a820bce38400e650476a62837598103f20
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-07-09 10:53:11 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1516 ms, Memory: 111344 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,22 @@
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.util.Arrays;
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine());
+        int [] arr = new int[A];
+        for(int i = 0; i < A; i++){
+            int B = Integer.parseInt(br.readLine());
+                   arr[i] = B;
+               }
+       Arrays.sort(arr);
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i : arr)
+            sb.append(i).append("\n");
+       System.out.println(sb);
+        
+    }
+}
