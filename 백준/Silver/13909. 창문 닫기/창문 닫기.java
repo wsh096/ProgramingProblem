fa424058d05b55188447559b91192ab1f85cc2ea
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-07-14 22:39:54 +0900

    [Silver V] Title: 창문 닫기, Time: 124 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -0,0 +1,9 @@
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println((int)Math.sqrt(Integer.parseInt(br.readLine())));
+    }
+}
