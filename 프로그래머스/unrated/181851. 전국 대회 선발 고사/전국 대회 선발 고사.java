5be4e85483f5b32eab6326a45adfac1f1222544f
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-07-23 16:01:21 +0900

    [unrated] Title: 전국 대회 선발 고사, Time: 1.54 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -1,15 +1,43 @@
-//쉬운 풀이 생각이 안 나네요 ㅠ
-import java.util.Arrays;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Collections;
+class Student{
+    int rank;
+    int idx;
+    boolean attendance;
+    Student(int rank, int idx, boolean attendance){
+        this.rank = rank;
+        this.idx = idx;
+        this.attendance = attendance;
+    }
+}
 class Solution {
     public int solution(int[] rank, boolean[] attendance) {
-       int[][] possible = new int[rank.length][2];
-        for(int[] i : possible) Arrays.fill(i,Integer.MAX_VALUE);
-        
-        for(int i = 0 ; i < rank.length; i++)
-            if(attendance[i]) possible[i] = new int[] {rank[i],i};                
-               
-       Arrays.sort(possible,(p1,p2)->p1[0]-p2[0]);
- 
-        return possible[0][1]*10000 + possible[1][1]*100 + possible[2][1];
+        List<Student> students = new LinkedList<>();
+        int answer = 0;
+        for(int i = 0; i < rank.length; i++){
+            students.add(new Student(rank[i],i,attendance[i]));            
+        }
+        Collections.sort(students, (a,b)-> a.rank-b.rank);
+        int cnt = 0;
+        for(Student student : students){
+            if(!student.attendance) continue;
+            else{
+                switch(cnt){
+                        case 0:
+                        answer += 10000 * student.idx;
+                        break;
+                        case 1:
+                        answer += 100 * student.idx; 
+                        break;
+                        case 2:
+                        answer += student.idx;
+                        break;
+                }
+                cnt++;
+            }
+            if(cnt == 3) break;
+        } 
+        return answer;
     }
 }
