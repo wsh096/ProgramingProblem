51782016e9322bd08a464f0d2acd48c7c1c12a9f
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-07-23 20:32:56 +0900

    [unrated] Title: 배열의 원소 삭제하기, Time: 0.10 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
+++ 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
@@ -1,36 +1,14 @@
-// //1. 일반 풀이
-// import java.util.List;
-// import java.util.ArrayList;
-// import java.util.Set;
-// import java.util.HashSet;
-// class Solution {
-//     public int[] solution(int[] arr, int[] delete_list) {
-//         Set<Integer> delete = new HashSet<>();//그냥 의미적으로 사용.
-//         List<Integer> list = new ArrayList<>();
-        
-//         for(int d : delete_list) delete.add(d);
-        
-//         for(int i = 0; i < arr.length; i++) {
-//             if(delete.contains(arr[i])) continue;
-//                 list.add(arr[i]);
-//         }
-        
-//         if(list.size()==arr.length) 
-//             return arr;
-        
-//         int[] answer = new int [list.size()]; 
-        
-//         for(int i = 0; i < answer.length; i++) 
-//             answer[i] = list.get(i);
-        
-//         return answer;
-//     }
-// }
-
-// //2. 스트림 풀이 
-import java.util.Arrays;
+import java.util.Set;
+import java.util.HashSet;
 class Solution {
     public int[] solution(int[] arr, int[] delete_list) {
-        return Arrays.stream(arr).filter(i -> Arrays.stream(delete_list).noneMatch(j -> j == i)).toArray();
+       Set<Integer> set = new HashSet<>();
+        for(int i : arr) set.add(i);
+        for(int i : delete_list) if(set.contains(i)) set.remove(i);
+        int[] answer = new int[set.size()];
+        int idx = 0;
+        for(int i = 0; i < arr.length; i++)
+            if(set.contains(arr[i])) answer[idx++] = arr[i];
+        return answer;
     }
 }
