6f2a44ba699076d421295f81047c831e2194a4cd
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-07-23 21:12:06 +0900

    [unrated] Title: 정수를 나선형으로 배치하기, Time: 0.06 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
+++ 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -1,17 +1,17 @@
 class Solution {
+    int[] dx = {0, 1 , 0 , -1};
+    int[] dy = {1, 0, -1, 0};
     public int[][] solution(int n) {
         int[][] answer = new int[n][n];
-        int[] dx = {0, 1, 0, -1};
-        int[] dy = {1, 0, -1, 0};
-        int x = 0, y = 0, d = 0;
-        for(int i = 1; i <= n*n; i++){
-            answer[x][y] = i;
-            int nx = x + dx[d];
-            int ny = y + dy[d];
+        int x = 0, y = 0, direction = 0, num = 1;
+        while(num <= n*n){
+            answer[x][y] = num++;
+            int nx = x+dx[direction];
+            int ny = y+dy[direction];
             if(nx < 0 || nx >= n || ny < 0 || ny >= n || answer[nx][ny] != 0){
-                d = (d + 1) % 4;
-                nx = x + dx[d];
-                ny = y + dy[d];
+                direction = (direction + 1) % 4;
+                nx = x + dx[direction];
+                ny = y + dy[direction];
             }
             x = nx;
             y = ny;
