45b443c755bb7058d691d9a64bfe6646e9418d60
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-07-23 16:15:37 +0900

    [unrated] Title: 전국 대회 선발 고사, Time: 2.85 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -1,43 +1,7 @@
-import java.util.List;
-import java.util.LinkedList;
-import java.util.Collections;
-class Student{
-    int rank;
-    int idx;
-    boolean attendance;
-    Student(int rank, int idx, boolean attendance){
-        this.rank = rank;
-        this.idx = idx;
-        this.attendance = attendance;
-    }
-}
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(int[] rank, boolean[] attendance) {
-        List<Student> students = new LinkedList<>();
-        int answer = 0;
-        for(int i = 0; i < rank.length; i++){
-            students.add(new Student(rank[i],i,attendance[i]));            
-        }
-        Collections.sort(students, (a,b)-> a.rank-b.rank);
-        int cnt = 0;
-        for(Student student : students){
-            if(!student.attendance) continue;
-            else{
-                switch(cnt){
-                        case 0:
-                        answer += 10000 * student.idx;
-                        break;
-                        case 1:
-                        answer += 100 * student.idx; 
-                        break;
-                        case 2:
-                        answer += student.idx;
-                        break;
-                }
-                cnt++;
-            }
-            if(cnt == 3) break;
-        } 
-        return answer;
-    }
+        return IntStream.range(0,rank.length).filter(i->attendance[i]).boxed().sorted((a,b)->rank[a]-rank[b]).limit(3).reduce((cur, next) -> cur*100+next).get();
+    }   
 }
