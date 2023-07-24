8116a46265fbf28a369fde11656c97519e2bb5a4
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-07-24 23:15:17 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 448 ms, Memory: 39984 KB -BaekjoonHub

--- 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -7,8 +7,8 @@ public class Main{
         int via = 6 - from - to;//중간을 들리는 역할
         //종료조건
         if(N == 1){
-            String fromTo = String.format("%d %d\n", from, to);
-            sb.append(fromTo);
+            //String fromTo = String.format("%d %d\n", from, to);
+            sb.append(from).append(" ").append(to).append("\n");
             return;
         }
         if(N >= 2){
@@ -20,8 +20,8 @@ public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        String times = String.format("%d\n", (1<<N)-1); 
-        sb.append(times);
+        //String times = String.format("%d\n", (1<<N)-1); 
+        sb.append((1<<N)-1).append("\n");
         hanoi(N, 1, 3);
         System.out.println(sb);
     }
