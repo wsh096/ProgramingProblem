ce1ddc4557927d6dbcd049467dd9d7bce8dcb29a
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-07-27 20:41:22 +0900

    [Silver III] Title: N과 M (1), Time: 284 ms, Memory: 23340 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static boolean[] visited;
+    static int[] selected;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N + 1];
+        selected = new int[M];
+
+        findSequence(1, 0);
+
+        System.out.println(sb.toString());
+
+        br.close();
+    }
+
+    private static void findSequence(int start, int depth) {
+        if (depth == M) {
+            for (int num : selected) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i <= N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                selected[depth] = i;
+                findSequence(1, depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
