3a51c258b0b22314a0d1e3775f06e29ee4a8d7e6
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-07-28 20:58:01 +0900

    [Silver III] Title: 알고리즘 수업 - 병합 정렬 1, Time: 632 ms, Memory: 80920 KB -BaekjoonHub

--- 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.java
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.java
@@ -2,8 +2,8 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.util.StringTokenizer;
-import java.util.Arrays;
-public class Main{
+
+public class Main {
     static int[] temp;
     static int count = 0;
     static int K;
@@ -16,23 +16,28 @@ public class Main{
 
         K = Integer.parseInt(st.nextToken());
         temp = new int[num];
-        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] arr = new int[num];
+		String[] input = br.readLine().split(" ");
+        for(int i = 0; i < num; i++){
+            arr[i] = Integer.parseInt(input[i]);
+        }
         
         merge_sort(arr, 0, num - 1);
         System.out.println(result);
     }
     private static void merge_sort(int[] A, int before, int after){
-        if(before < after){
-            int mid = before + (after - before)/2;
-            merge_sort(A, before, mid);
+        if(before < after){ 
+            int mid = before + (after - before)/2; 
+            merge_sort(A, before, mid); 
             merge_sort(A, mid + 1, after);
-            merge(A, before, mid, after);
+            merge(A, before, mid, after); 
         }
     }
+
     private static void merge(int[] A, int before, int mid, int after){
-        int i = before;
+        int i = before; 
         int j = mid + 1;
-        int t = 0;
+        int t = before;
         
         while(i <= mid && j <= after){
             if(A[i] <= A[j]){
@@ -41,22 +46,23 @@ public class Main{
                 temp[t++] = A[j++];
             }
         }
+		
         while(i <= mid){
             temp[t++] = A[i++];
         }
+				
         while(j <= after){
             temp[t++] = A[j++];
-        }
-        t = 0;
-        i = before;
-        
-        while(i <= after){
-            count++;
-            if(count == K){
-                result = temp[t];
-                break;
-            }
-            A[i++] = temp[t++];
-        }
+        }			
+	
+                for(int k = before; k <= after; k++){
+                    count++;
+                    if(count == K){
+                        A[k] = temp[k];
+                        result = temp[k];
+                        break;
+                    }
+                    A[k] = temp[k];
+                }
     }
 }
