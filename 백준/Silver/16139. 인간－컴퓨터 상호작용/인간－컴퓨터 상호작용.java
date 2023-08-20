f29d193d75c93d0a08cb46e02d5dd0cc4d4fdb6c
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-08-20 09:23:14 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 776 ms, Memory: 125660 KB -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -8,13 +8,21 @@ public class Main{
         StringBuilder answer = new StringBuilder();
         char[] origin = br.readLine().toCharArray();
         int n = Integer.parseInt(br.readLine());
+        
+        int[][] cumulativeSum = new int[26][origin.length + 1];
+        for(int i = 0; i < origin.length; i++){
+            for(int j = 0; j < 26; j++){
+                cumulativeSum[j][i + 1] = cumulativeSum[j][i];
+            }
+             cumulativeSum[origin[i] - 'a'][i + 1]++;   
+        }
         for(int i = 0; i < n; i++){
             String[] input = br.readLine().split(" ");
             char c = input[0].charAt(0);
             int l = Integer.parseInt(input[1]);
             int r = Integer.parseInt(input[2]);
-            int temp = 0;
-            for(int j = l; j <= r; j++) if(origin[j] == c) temp++;
+            
+            int temp = cumulativeSum[c - 'a'][r + 1] - cumulativeSum[c - 'a'][l];
             answer.append(temp).append("\n");
         }
         System.out.println(answer);
