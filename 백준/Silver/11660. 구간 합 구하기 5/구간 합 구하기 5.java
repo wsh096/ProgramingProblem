76efbfc64b1081947d8fd449a4f32f9ba2d3082d
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-08-21 17:35:32 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 860 ms, Memory: 117552 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -18,13 +18,12 @@ public class Main{
         StringBuilder sb = new StringBuilder();
         int n = makeInt(st.nextToken());
         int m = makeInt(st.nextToken());
-        int[][] board = new int[n + 1][n + 1];
+        
         int[][] sum = new int[n + 1][n + 1];
         for(int i = 1; i <= n; i++){
             st = makeToken(br.readLine());
             for(int j = 1; j <= n; j++){
-                board[i][j] = makeInt(st.nextToken());
-                sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + board[i][j];
+                sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + makeInt(st.nextToken());
             }
         }
         for(int i = 0; i < m; i++){
