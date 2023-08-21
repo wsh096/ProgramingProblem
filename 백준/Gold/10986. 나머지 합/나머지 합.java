9e416331bb8c3398154e49097a38fcf0f9915778
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-08-21 13:37:27 +0900

    [Gold III] Title: 나머지 합, Time: 484 ms, Memory: 112944 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -10,17 +10,19 @@ public class Main{
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
-        int[] sum = new int[n + 1];
-        int[] arr = new int[n + 1];
-        int[] remainder = new int[m];
+        
+        int[] remainder = new int[m]; // 나머지 값 같은 값 구하기.
+        int sum = 0;
+        int num = 0;
+        
         for(int i = 1; i <= n; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
-            sum[i] = (sum[i-1] + arr[i]) % m;
-            remainder[(sum[i] + m) % m]++; 
+            num = Integer.parseInt(st.nextToken()); // 1. 배열에 집어 넣고
+            sum = (sum + num) % m; // 2. sum 계산(처음것도 계산하기 위해, 1부터 시작)
+            remainder[sum]++;// 3. 누적합의 나머지 개수 구하기.
         }
         long result = remainder[0];
         for(int i = 0; i < m; i++){
-            result += (long)remainder[i] * (remainder[i] - 1) / 2;
+            result += (long)remainder[i] * (remainder[i] - 1) / 2; // (1~j)%M == (1~i-1)%M 두 개의 경우를 가지는 경우의 수.
         }
         System.out.println(result);
     }
