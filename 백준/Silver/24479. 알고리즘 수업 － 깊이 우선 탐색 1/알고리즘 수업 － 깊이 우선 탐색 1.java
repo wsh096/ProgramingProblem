d8631b0c559375d2a308c6cad0d28fac1e3f54c8
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-09-17 16:12:12 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 1140 ms, Memory: 117140 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    static int[] visited;
+    static List<Integer>[] line;
+    static int cnt;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
+        int R = Integer.parseInt(input[2]);
+
+        line = new List[N + 1];
+        for (int i = 1; i <= N; i++) {
+            line[i] = new ArrayList<>();
+        }
+
+        visited = new int[N + 1];
+        cnt = 1;
+
+        for (int i = 0; i < M; i++) {
+            input = br.readLine().split(" ");
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
+            line[a].add(b);
+            line[b].add(a);
+        }
+
+        dfs(R);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            sb.append(visited[i]).append("\n");
+        }
+        System.out.print(sb.toString());
+    }
+
+    static void dfs(int t) {
+        visited[t] = cnt;
+        Collections.sort(line[t]);
+
+        for (int i : line[t]) {
+            if (visited[i] == 0) {
+                cnt++;
+                dfs(i);
+            }
+        }
+    }
+}
