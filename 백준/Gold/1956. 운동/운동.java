e3b1870f39ca5c9f78c7f494abe05d94ab30aaac
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-10-04 13:45:20 +0900

    [Gold IV] Title: 운동, Time: 768 ms, Memory: 67736 KB -BaekjoonHub

---
+++ 백준/Gold/1956. 운동/운동.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+ 
+public class Main {
+    static final int INF = 987654321;
+ 
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+ 
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+ 
+        int[][] arr = new int[V + 1][V + 1];
+ 
+        for (int i = 1; i <= V; i++) {
+            for (int j = 1; j <= V; j++) {
+                if (i != j) {
+                    arr[i][j] = INF;
+                }
+            }
+        }
+ 
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+ 
+            arr[a][b] = c;
+        }
+ 
+        for (int k = 1; k <= V; k++) {
+            for (int i = 1; i <= V; i++) {
+                for (int j = 1; j <= V; j++) {
+                    if (i == j) {
+                        continue;
+                    }
+ 
+                    if (arr[i][j] > arr[i][k] + arr[k][j]) {
+                        arr[i][j] = arr[i][k] + arr[k][j];
+                    }
+                }
+            }
+        }
+ 
+        int ans = INF;
+        for (int i = 1; i <= V; i++) {
+            for (int j = 1; j <= V; j++) {
+                if (i == j) {
+                    continue;
+                }
+ 
+                if (arr[i][j] != INF && arr[j][i] != INF) {
+                    ans = Math.min(ans, arr[i][j] + arr[j][i]);
+                }
+            }
+        }
+
+        ans = (ans == INF) ? -1 : ans;
+ 
+        bw.write(ans + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+ 
+}
