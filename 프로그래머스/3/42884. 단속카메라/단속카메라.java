921284c570382fba4561684dc41f013b34233076
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-10-12 22:29:20 +0900

    [level 3] Title: 단속카메라, Time: 8.28 ms, Memory: 58.6 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[][] routes) {
         int answer = 0;
-        Arrays.sort(routes, (a,b) -> a[1] - b[1]); // 차량이 나간 시점 기준으로 오름차순 정렬.
+        Arrays.sort(routes, (a, b) -> a[1] - b[1]); // 차량이 나간 시점 기준으로 오름차순 정렬.
         int camera = Integer.MIN_VALUE; //카메라 설치를 위한 값 갱신 초기값
         for(int[] route : routes){
             if(camera < route[0]){//시작점 보다 작으면
