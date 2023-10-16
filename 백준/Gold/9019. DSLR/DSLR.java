464a7638bd97303c66a1f4fec3393618ba23e541
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-10-17 07:18:31 +0900

    [Gold IV] Title: DSLR, Time: 948 ms, Memory: 18396 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.java
+++ 백준/Gold/9019. DSLR/DSLR.java
@@ -1,50 +1,81 @@
-import java.io.*;
-import java.util.*;
-public class Main{
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        
-        for(int i = 0; i < n; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            String[] cmd = new String[10000];
-            boolean[] visited = new boolean[10000];
-            Queue<Integer> queue = new LinkedList<>();
-            
-            visited[a] = true;
-            queue.offer(a);
-            Arrays.fill(cmd,"");
-            while(!queue.isEmpty() && !visited[b]){
-                int now = queue.poll();
-                int D = (2 * now) % 10000;
-                int S = (now == 0) ? 9999 : now - 1;
-                int L = (now % 1000) * 10 + now/1000;
-                int R = (now % 10) * 1000 + now/10;
-                
-                if(!visited[D]){
-                    queue.offer(D);
-                    visited[D] = true;
-                    cmd[D] = cmd[now] + "D";
+class Main {
+
+    static final int MAX = 10_000;
+    static final char D = 'D', S = 'S', L = 'L', R = 'R';
+
+    static StringBuilder answer;
+
+    static int[] parent;
+    static char[] operator;
+
+    static final int MASK = (1 << 12) - 1;
+    static int[] queue;
+    static int head, tail;
+
+    public static void main(String[] args) throws Exception {
+
+        int T = read();
+        answer = new StringBuilder();
+
+        queue = new int[MASK + 1];
+        parent = new int[MAX];
+        operator = new char[MAX];
+
+        while (T-- > 0) bfs(read(), read());
+
+        System.out.print(answer);
+
+    }
+
+    static final int BUFFER_SIZE = 14;
+    static int bufferIndex = 0;
+    static char[] buffer = new char[BUFFER_SIZE];
+
+    static void bfs(int number, int target) {
+        ready();
+
+        parent[number] = queue[++head] = number;
+
+        while (head > tail) {
+
+            int n = queue[++tail & MASK];
+
+            run(D, n, (n << 1) % MAX);
+            run(S, n, n == 0 ? 9999 : n - 1);
+            run(L, n, n % 1000 * 10 + n / 1000);
+            run(R, n, n / 10 + n % 10 * 1000);
+
+            if (parent[target] >= 0) {
+                bufferIndex = BUFFER_SIZE;
+                while (target != number) {
+                    buffer[--bufferIndex] = operator[target];
+                    target = parent[target];
                 }
-                if(!visited[S]){
-                    queue.offer(S);
-                    visited[S] = true;
-                    cmd[S] = cmd[now] + "S";
-                }
-                if(!visited[L]){
-                    queue.offer(L);
-                    visited[L] = true;
-                    cmd[L] = cmd[now] + "L";
-                }
-                if(!visited[R]){
-                    queue.offer(R);
-                    visited[R] = true;
-                    cmd[R] = cmd[now] + "R";
-                }
+                answer
+                    .append(buffer, bufferIndex, BUFFER_SIZE - bufferIndex)
+                    .append('\n');
+                return;
             }
-            System.out.println(cmd[b]);
+
         }
     }
+
+    static void run(char o, int p, int n) {
+        if (parent[n] >= 0) return;
+        parent[n] = p;
+        operator[n] = o;
+        queue[++head & MASK] = n;
+    }
+
+    static void ready() {
+        head = tail = -1;
+        for (int i = 0; i < MAX; i++) parent[i] = -1;
+    }
+
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+
 }
