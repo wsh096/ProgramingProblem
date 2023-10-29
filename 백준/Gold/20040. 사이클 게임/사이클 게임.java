ac9697360ee92a240bad338f0f9fe3ea1e916e93
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-10-29 18:02:30 +0900

    [Gold IV] Title: 사이클 게임, Time: 696 ms, Memory: 124628 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+ 
+public class Main {
+ 
+    static int N, M, res, parents[];
+    
+    public static void main(String[] args) throws IOException {
+ 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+ 
+        parents = new int[N];
+        for (int i = 0; i < N; i++) {
+            parents[i] = i;
+        }
+        
+        for (int i = 1; i <= M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+ 
+            if(!union(a, b)) {
+                res = i;
+                break;
+            }
+        }
+        
+        System.out.println(res);
+    }
+ 
+    private static boolean union(int a, int b) {
+        int aRoot = find(a); 
+        int bRoot = find(b); 
+
+        if(aRoot == bRoot) return false;
+        parents[bRoot] = aRoot;
+        return true;
+    }
+ 
+    private static int find(int n) {
+        if(n == parents[n]) return n;
+        return parents[n] = find(parents[n]);
+    }
+ 
+}
