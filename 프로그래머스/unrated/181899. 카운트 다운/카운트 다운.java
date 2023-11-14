86e3c9c7c579e295a55ce96cba398868a2b5d38d
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-11-14 19:29:05 +0900

    [level unrated] Title: 카운트 다운, Time: 0.03 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
@@ -1,7 +1,9 @@
-import java.util.stream.IntStream;
-
 class Solution {
-    public int[] solution(int start, int end) {
-        return IntStream.iterate(start,i->i-1).limit(start-end+1).toArray();
+    public int[] solution(int start, int end_num) {
+        int[] answer = new int[start - end_num + 1];
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = start - i;
+        }
+        return answer;
     }
 }
