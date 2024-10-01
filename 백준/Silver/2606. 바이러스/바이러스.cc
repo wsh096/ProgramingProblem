53d16d505c9f0584f5ea4aad225c033e89925b33
Wang_Seok_Hyeon <wsh096@gmail.com>
2024-10-01 22:01:56 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.cc
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -1,40 +1,34 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-
 using namespace std;
-
-int main() {
+int main(void){
+    int answer = 0;
     int N, M;
     cin >> N >> M;
-
-    vector<vector<int>> graph(N + 1); 
-
-    for (int i = 0; i < M; i++) {
+    vector<vector<int>> graph(N + 1);
+    for(int i = 0; i < M; i++){
         int x, y;
-        cin >> x >> y; 
+        cin >> x >> y;
         graph[x].push_back(y);
         graph[y].push_back(x);
-    }
-
+    }    
     vector<bool> visited(N + 1, false);
     queue<int> q;
-    q.push(1); 
+    q.push(1);
     visited[1] = true;
-    int answer = 0;
-
-    while (!q.empty()) {
+    while(!q.empty()){
         int cur = q.front();
         q.pop();
-        for (int neighbor : graph[cur]) {
-            if (!visited[neighbor]) {
-                visited[neighbor] = true;
-                q.push(neighbor);
+        for(int next : graph[cur]){
+            if(!visited[next]){
+                visited[next] = true;
+                q.push(next);
                 answer++;
             }
         }
     }
-
+    
     cout << answer << endl;
     return 0;
 }
