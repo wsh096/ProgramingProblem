cb34d640568fd72a95d84afd379b87508a438074
Wang_Seok_Hyeon <wsh096@gmail.com>
2024-10-01 19:34:55 +0900

    [Silver I] Title: 미로 탐색, Time: 196 ms, Memory: 18500 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,56 +1,48 @@
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
-
-public class Main {
+import java.util.Queue;
+import java.util.LinkedList;
+public class Main{
     static int N, M;
-    static int[][] maze;
     static boolean[][] visited;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-    
-    public static void main(String[] args) {
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
         M = sc.nextInt();
-        maze = new int[N][M];
+        sc.nextLine();
         visited = new boolean[N][M];
-        for (int i = 0; i < N; i++) {
-            String input = sc.next();
-            for (int j = 0; j < M; j++) {
-                maze[i][j] = input.charAt(j) - '0';
-            }
-        }
-        sc.close();
         
-        bfs(0, 0);
-        System.out.println(maze[N-1][M-1]);
-    }
-    
-    private static void bfs(int x, int y) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {x, y});
-        visited[x][y] = true;
-        
-        while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            int curX = cur[0];
-            int curY = cur[1];
-            
-            for (int i = 0; i < 4; i++) {
-                int nx = curX + dx[i];
-                int ny = curY + dy[i];
-                
-                if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
-                    continue;
-                }
-                
-                if (maze[nx][ny] == 1 && !visited[nx][ny]) {
-                    queue.offer(new int[] {nx, ny});
-                    visited[nx][ny] = true;
-                    maze[nx][ny] = maze[curX][curY] + 1;
+        for(int i = 0; i < N; i++){
+            char[] temp = sc.nextLine().toCharArray();
+            for(int j = 0; j < M; j++){
+                if(temp[j] == '0'){
+                    visited[i][j] = true;
+                }else{
+                    visited[i][j] = false;
                 }
             }
         }
+    //위치 정보와 현재 이동값.
+    Queue<int[]> queue = new LinkedList<>();
+    queue.offer(new int[] {0, 0, 1});
+    visited[0][0] = true;
+    while(!queue.isEmpty()){
+        int[] temp = queue.poll();
+        if(temp[0] == (N - 1) && temp[1] == (M -1)){
+            System.out.println(temp[2]);
+            break;
+        }
+        for(int i = 0; i < 4; i++){
+            int nx = temp[0] + dx[i];
+            int ny = temp[1] + dy[i];
+            if(nx < 0 || nx >= N || ny < 0 || ny >= M || visited[nx][ny] == true){
+                continue;
+            }else{
+                queue.offer(new int[] {nx, ny, temp[2] + 1});
+                visited[nx][ny] = true;
+            }
+        }
     }
+}
 }
