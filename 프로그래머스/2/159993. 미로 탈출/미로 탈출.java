c8a7d9e717ae3a5293571fba3b5cf2083cc0c72a
Wang_Seok_Hyeon <wsh096@gmail.com>
2024-10-02 15:24:48 +0900

    [level 2] Title: 미로 탈출, Time: 7.30 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -3,43 +3,47 @@ import java.util.Queue;
 
 class Solution {
     public int solution(String[] maps) {
-        boolean[][] visited = new boolean[maps.length][maps[0].length()];
+        
         int[] s = new int[3];
         int[] l = new int[3];
         int[] e = new int[3];
-        for (int i = 0; i < maps.length; i++) {
+        int cnt = 0;
+exit_l: for (int i = 0; i < maps.length; i++) {
             for (int j = 0; j < maps[0].length(); j++) {
                 char temp = maps[i].charAt(j);
+                if(cnt >= 3){
+                    break exit_l;
+                }
                 switch (temp) {
                     case 'S':
                         s[0] = i;
                         s[1] = j;
-                        s[2] = 0;
+                        cnt++;
                         break;
                     case 'L':
                         l[0] = i;
                         l[1] = j;
-                        l[2] = -1;
+                        cnt++;
                         break;
                     case 'E':
                         e[0] = i;
                         e[1] = j;
-                        e[2] = -1;
+                        cnt++;
                         break;
                 }
             }
         }
-        l[2] = miro(s, l, visited, maps);
+        l[2] = miro(s, l, maps);
         if(l[2] == -1){
             return -1;
         }
-         visited = new boolean[maps.length][maps[0].length()];
-         return miro(l, e, visited, maps);
+         return miro(l, e, maps);
         }
     
-    private int miro(int[] start, int[] target, boolean[][] visited, String[] maps) {
-        int[] dx = { 0, 1, 0, -1 };
-        int[] dy = { 1, 0, -1, 0 };
+    private int miro(int[] start, int[] target, String[] maps) {
+        boolean[][] visited = new boolean[maps.length][maps[0].length()];
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
         Queue<int[]> bfs = new LinkedList<>();
         bfs.offer(start);
         visited[start[0]][start[1]] = true;
