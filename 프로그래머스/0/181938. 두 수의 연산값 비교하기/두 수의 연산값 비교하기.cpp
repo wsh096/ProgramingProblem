27f780454ca91f12b9b6bd9713d44b40bc376f69
Wang_Seok_Hyeon <wsh096@gmail.com>
2024-10-04 23:26:24 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 0.02 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.cpp
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.cpp
@@ -4,5 +4,5 @@
 using namespace std;
 
 int solution(int a, int b) {
-    return max(atoi((to_string(a) + to_string(b)).c_str()), 2 * a * b);
+    return max(stoi(to_string(a) + to_string(b)), 2 * a * b);
 }
