727ca6af5d04cef375a2f6492f87a9830fb681e3
Wang_Seok_Hyeon <wsh096@gmail.com>
2024-10-04 22:44:59 +0900

    [level 0] Title: 수 조작하기 1, Time: 1.28 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.cpp
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.cpp
@@ -1,17 +1,12 @@
 #include <string>
 #include <vector>
-#include <utility>
+#include <map>
 using namespace std;
 
 int solution(int n, string control) {
-    vector<pair<char, int>> changes = {{'w', 1}, {'s', -1}, {'d', 10}, {'a', -10}};
+    map<char, int> changes = {{'w', 1}, {'s', -1}, {'d', 10}, {'a', -10}};
     for(char c : control){
-        for(auto change : changes){
-            if(change.first == c){
-                n += change.second;
-                break;
+         n += changes[c];
             }
-        }
-    }
     return n;
 }
