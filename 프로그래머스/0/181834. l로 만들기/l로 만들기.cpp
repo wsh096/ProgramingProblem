e0a74604adc9c3871143fc515a4f3b322c338986
Wang_Seok_Hyeon <wsh096@gmail.com>
2024-10-05 10:39:31 +0900

    [level 0] Title: l로 만들기, Time: 0.35 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    string answer = "";
+    for(char c : myString){
+       answer += (c < 'l') ? 'l' : c;
+    }
+    return answer;
+}
