d8efd02e5c2d1861467fa0603b61b88785ab8aa1
Wang_Seok_Hyeon <wsh096@gmail.com>
2024-10-05 17:46:27 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 0.35 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
+    for(vector<int> q : queries){
+        for(int i = q[0]; i <= q[1]; i++){
+            arr[i]++;
+        }
+    }
+    return arr;
+}
