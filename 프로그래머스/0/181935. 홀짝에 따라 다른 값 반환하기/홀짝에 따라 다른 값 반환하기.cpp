6cabc9163dcd4f97eea90617b240973971e08739
Wang_Seok_Hyeon <wsh096@gmail.com>
2024-10-05 15:26:37 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.cpp
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.cpp
@@ -6,18 +6,10 @@ using namespace std;
 
 int solution(int n) {
     int answer = 0;
-    if(n % 2 == 0){
         while(n > 0){
-            answer += pow(n, 2);
+            answer += n % 2 == 0 ? pow(n, 2) : n;
                 n -= 2;
         }
-    }
-    else{
-        while(n > 0){
-            answer += n;
-            n -= 2;
-        }
-    }
     
     return answer;
 }
