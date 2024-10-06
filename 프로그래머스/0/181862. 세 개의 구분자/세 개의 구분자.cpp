89e11c0ba15032e07837c989d064e830e1a33454
Wang_Seok_Hyeon <wsh096@gmail.com>
2024-10-06 12:31:17 +0900

    [level 0] Title: 세 개의 구분자, Time: 41.72 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+vector<string> solution(string myStr) {
+    vector<string> answer;
+    for(char& c : myStr){
+        if(c == 'a' || c == 'b' || c == 'c')c = ' ';
+    }
+    string str;
+    stringstream ss;
+    ss.str(myStr);
+    while(ss >> str){
+        answer.emplace_back(str);
+    }
+    if(answer.empty())  answer.emplace_back("EMPTY");
+    return answer;
+}
