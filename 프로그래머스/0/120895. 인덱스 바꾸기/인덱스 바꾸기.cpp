df21b52f4338bee0f4f838e7390ff0729ebd7915
Wang_Seok_Hyeon <wsh096@gmail.com>
2024-10-09 22:59:35 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.03 ms, Memory: 3.64 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.cpp
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 string solution(string my_string, int num1, int num2) {
+    /* swap은 void 타입이다. string class에 포함되어 있다. */
     swap(my_string[num1], my_string[num2]);
     return my_string;
 }
