56bb6223c650b47c9544272ea52f082879b0a3d4
Wang_Seok_Hyeon <wsh096@gmail.com>
2024-10-17 08:28:49 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
@@ -3,20 +3,17 @@
 
 using namespace std;
 
-int gcd(int a,int b){
+int gcd(int a, int b){
     if(b == 0){
         return a;
     }
     return gcd(b, a % b);
 }
 int lcm(int a, int b){
-    return (a * b) / gcd(a, b); //최대 공약수lcm = (a*b) / (a, b)의 최소 공배수
+    return (a * b) / gcd(a, b);
 }
-    
 int solution(vector<int> arr) {
-    //정렬 없이 진행
     int answer = arr[0];
-    if(arr.size() == 1) return arr[0];
     for(int i = 1; i < arr.size(); i++){
         answer = lcm(answer, arr[i]);
     }
