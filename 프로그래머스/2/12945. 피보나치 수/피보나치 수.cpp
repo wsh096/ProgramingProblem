49edfa399b784d82097708b159056a48ad115b6d
Wang_Seok_Hyeon <wsh096@gmail.com>
2024-10-22 17:23:13 +0900

    [level 2] Title: 피보나치 수, Time: 0.54 ms, Memory: 4.28 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+int dp[100001];
+int solution(int n) {
+    dp[1] = 1;
+    for(int i = 2; i <= n; i++){
+        dp[i] = (dp[i - 2] + dp[i - 1])%1234567;
+    }
+    return dp[n];
+}
